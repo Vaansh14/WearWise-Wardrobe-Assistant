df8aba589361bb965ed5fef1fd7aa028fd749e26
vaansh1902 <vaansh.bhatia@city.ac.uk>
2026-03-15 21:14:54 +0530

    initial commit

---
+++ WearWise-Backend/src/main/java/com/example/wearwise/WearWiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.wearwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearWiseApplication.class, args);
+	}
+
+}
---
+++ WearWise-Backend/src/test/java/com/example/wearwise/WearWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wearwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
